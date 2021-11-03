404d6189ab8c645b0a8fc7405ab1ca5faa4ee715
kgtkr <contact@kgtkr.net>
2021-11-03 16:26:54 +0900

    fix ci

--- deps.sh
+++ deps.sh
@@ -1,22 +1,35 @@
 #!/bin/sh -eu
+CACHE_DIR=cache
 
-VERSION=4.0b2
-TGZ_CACHE_DIR=cache/processing.tgz
-CACHE_DIR=cache/processing
-
-mkdir -p $TGZ_CACHE_DIR
-
-if [ ! -f $TGZ_CACHE_DIR/$VERSION ]; then
-    wget https://github.com/processing/processing4/releases/download/processing-1277-4.0b2/processing-$VERSION-linux64.tgz -O $TGZ_CACHE_DIR/$VERSION
+BIN_TGZ_CACHE_DIR=$CACHE_DIR/processing-tgz
+mkdir -p $BIN_TGZ_CACHE_DIR
+if [ ! -e $BIN_TGZ_CACHE_DIR/$TAG ]; then
+    BIN_URL=https://github.com/processing/$REPO/releases/download/$TAG/$ASSET_NAME
+    wget $BIN_URL -O $BIN_TGZ_CACHE_DIR/$TAG
 fi
 
-mkdir -p $CACHE_DIR
-
-if [ ! -e $CACHE_DIR/$VERSION ]; then
-    tar -xzf $TGZ_CACHE_DIR/$VERSION -C $CACHE_DIR
-    mv $CACHE_DIR/processing-$VERSION $CACHE_DIR/$VERSION
+BIN_CACHE_DIR=$CACHE_DIR/processing
+mkdir -p $BIN_CACHE_DIR
+if [ ! -e $BIN_CACHE_DIR/$TAG ]; then
+    tar -xzf $BIN_TGZ_CACHE_DIR/$TAG -C $BIN_CACHE_DIR
+    mv $BIN_CACHE_DIR/processing-$VERSION $BIN_CACHE_DIR/$TAG
 fi
 
 rm -rf lib
 mkdir lib
-cp $CACHE_DIR/$VERSION/lib/*.jar $CACHE_DIR/$VERSION/core/library/*.jar $CACHE_DIR/$VERSION/modes/java/mode/*.jar lib
+cp $BIN_CACHE_DIR/$TAG/lib/*.jar $BIN_CACHE_DIR/$TAG/core/library/*.jar $BIN_CACHE_DIR/$TAG/modes/java/mode/*.jar lib
+
+SRC_TGZ_CACHE_DIR=$CACHE_DIR/processing-src-tgz
+mkdir -p $SRC_TGZ_CACHE_DIR
+if [ ! -e $SRC_TGZ_CACHE_DIR/$TAG ]; then
+    SRC_URL=https://github.com/processing/$REPO/archive/refs/tags/$TAG.tar.gz
+    wget $SRC_URL -O $SRC_TGZ_CACHE_DIR/$TAG
+fi
+
+SRC_CACHE_DIR=$CACHE_DIR/processing-src
+mkdir -p $SRC_CACHE_DIR
+if [ ! -e $SRC_CACHE_DIR/$TAG ]; then
+    tar -xzf $SRC_TGZ_CACHE_DIR/$TAG -C $SRC_CACHE_DIR
+    mv $SRC_CACHE_DIR/$REPO-$TAG $SRC_CACHE_DIR/$TAG
+fi
+
--- patch.sh
+++ patch.sh
@@ -1,21 +1,6 @@
 #!/bin/sh -eu
-
-COMMIT_ID=e6c1c0c45a02baf744cf02522eb83eb3b6ccc542
-TGZ_CACHE_DIR=cache/processing4-src.tgz
-CACHE_DIR=cache/processing4-src
-
-mkdir -p $TGZ_CACHE_DIR
-
-if [ ! -f $TGZ_CACHE_DIR/$COMMIT_ID ]; then
-    wget https://github.com/processing/processing4/archive/$COMMIT_ID.tar.gz -O $TGZ_CACHE_DIR/$COMMIT_ID
-fi
-
-mkdir -p $CACHE_DIR
-
-if [ ! -e $CACHE_DIR/$COMMIT_ID ]; then
-    tar -xzf $TGZ_CACHE_DIR/$COMMIT_ID -C $CACHE_DIR
-    mv $CACHE_DIR/processing4-$COMMIT_ID $CACHE_DIR/$COMMIT_ID
-fi
+CACHE_DIR=cache
+SRC_CACHE_DIR=$CACHE_DIR/processing-src
 
 resolve_patch () {
     MODE=$1
@@ -23,7 +8,7 @@ resolve_patch () {
     PATCHED=$3
     PATCH=$4
 
-    SRC_PATH=$CACHE_DIR/$COMMIT_ID/$SRC
+    SRC_PATH=$SRC_CACHE_DIR/$TAG/$SRC
     PATCHED_PATH=src/main/java/$PATCHED
     PATCH_PATH=patches/$PATCH
     PATCHED_DIR=$(dirname $PATCHED_PATH)
